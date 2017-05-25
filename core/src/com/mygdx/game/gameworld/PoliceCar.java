15b4277b0f6b2268ba44f3f7da8bb9adffd18962
Sasha <s.sasha44@yahoo.com>
2017-05-25 20:17:00 +0300

    Added Icon and Initialize Police Car

---
+++ core/src/com/mygdx/game/gameworld/PoliceCar.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.gameworld;
+
+/**
+ * Created by hackintosh on 5/25/17.
+ */
+
+public class PoliceCar extends Car {
+
+    PoliceCar(int type, int frameNumber, float angle, CarMoveDirection moveDir) {
+        super(type, frameNumber, angle, moveDir);
+
+    }
+}
